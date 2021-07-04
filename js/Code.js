89e965ed7e8f54a3dad54d38aade842103a21191
Monish-123 <80194930+Monish-123@users.noreply.github.com>
2021-07-04 11:50:59 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIABLE";
+const accessCode3 = "FUNCTION";
+const accessCode2 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+text("C T F N U I N O", 100,250)
+fill("lightblue")
+text("Hint: Performs a particular task !!", 100,270)
+fill("white")
+text("T A B S A D A E", 700,160)
+fill("lightblue")
+text("Hint: Stores Information !!", 700,180)
+
+}
